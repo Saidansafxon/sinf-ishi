fd1e8d538ca2bd3a1cd772cbc7eef681dcd59e17
Saidansafxon <saida01021987@gmail.com>
2025-11-03 20:54:10 +0500

    Add files via upload

---
+++ sai/main.py
@@ -0,0 +1,98 @@
+#6
+list1=[34,56,432,4333]
+def yigindi(list1)->int:
+    return sum(list1)
+print(yigindi)
+
+#7
+sa="nj jfnewj njfnewi"
+def matn(sa)->str:
+    print(len(sa))
+print(matn)
+
+#8
+sa1=int(input("son kiriting: "))
+def faktorial()->int:
+    for qol in range()
+
+#9 qilish keremas
+
+#10
+list2=[46]
+def juft_son()->int:
+    if list2/2:
+        print("juft")
+    else:
+        print("toq")
+
+#11
+sa3="kuchuk"
+def palindrom():
+    if sa3:
+        print(soz palindrom)
+    else:
+        print("palindrom emas")
+
+#12
+son1=int(input("son kiriting"))
+son2=int(input("son kiriting"))
+def daraja(son1,son2)->int:
+    pow(son1,son2)
+daraja
+
+#13
+ism="mirusmon","akbar","mirjahon".
+def katta_harf():
+    ism.capitalize()
+print(katta_harf)
+
+#14
+list5=[32,543223,4234,3213]
+def qiymat(list5):
+    return max(list5)
+print(qiymat)
+#15
+def qiymat(royhat):
+    natija = []
+    for narsa in royhat:
+        if royhat.count(narsa) == 1:
+            natija.append(narsa)
+    return natija
+
+#16
+matn1="hwk ghebkthgtk htkbe "
+def matn2(matn1):
+    print(len(matn1))
+#17
+list9=[45,56,34]
+def sa(list9):
+    print(45+56+34/3)
+print(sa)
+#18
+matn5="rgdt hrj bkgwhkj fbwhr"
+matn6="kgfd  hrkhw rrs"
+def sa1(matn5,matn6):
+    print(matn5+matn6)
+print(sa1)
+#19
+sa3=45,346,34,54
+def sa4(sa3):
+    if sa3>0:
+        print("musbat")
+    else:
+        print("manfiy")
+#20
+def raqamlar_yigindisi(son):
+    yigindi = 0
+    son = abs(son) 
+    while son > 0:
+        yigindi += son % 10  
+        son //= 10            
+    return yigindi
+#22
+hef=" wueg4yg y4g3u u4"
+hg="ieugy rwiuw u ew"
+be=hef+hg
+def heu(be):
+    return heu.differenze
+#23
